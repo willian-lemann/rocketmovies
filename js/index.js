1fcae481139ecff886456535875db4b16720b48f
Willian Lemann <willianlemann@hotmail.com>
2019-09-06 10:42:08 -0300

    final

--- js/index.js
+++ js/index.js
@@ -8,18 +8,63 @@ let lastPage = 0;
 let index = 1;
 let jsonContent = [];
 
-
 $(document).ready(function () {
     getSearch();
     M.updateTextFields();
 });
 
-
+document.getElementById('btnPesquisar').addEventListener('click', () => {
+    getMovies();
+});
 
 document.getElementById('btnDownload').addEventListener('click', () => {
     download(`${movieName.value}.csv`, ConvertToCSV(JSON.stringify(jsonContent)));
 });
 
+document.getElementById('searchInput').addEventListener('keydown', (e) => {
+
+    document.getElementById('cards').innerHTML = '';
+    if (e.keyCode === 8) {
+        index = 1;
+        pages = 1;
+        lastItem = '';
+        document.getElementById('pagination').innerHTML = '';
+    }
+
+    if (e.keyCode === 13)
+        getMovies();
+});
+
+function ConvertToCSV(objArray) {
+
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+
+    for (let i = 0; i < array.length; i++) {
+
+        let line = '';
+        for (let index in array[i]) {
+
+            if (line != '')
+                line += ','
+
+            line += array[i][index];
+        }
+        str += line + '\r\n';
+    }
+    return str;
+}
+
+function download(filename, text) {
+
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
 
 let message = (message, time, classe) => {
     M.toast({
@@ -33,7 +78,6 @@ let message = (message, time, classe) => {
 let validaCampo = (value) => {
     value == '' ? message('Filme não encontrado ou campo vazio', 200, 'rounded') : {}
 }
-//https://www.imdb.com/title/%7Bimdbid%7D
 
 let loadPage = (id) => {
 
@@ -65,11 +109,13 @@ let loadPage = (id) => {
 
 let loadModal = (id) => {
 
-    axios.get(`http://www.omdbapi.com/?i=${id.id}&apikey=27ec0df9&type=movie`)
+    let movieID = id.id;
+    axios.get(`http://www.omdbapi.com/?i=${movieID}&apikey=27ec0df9&type=movie`)
         .then((res) => {
 
             let data = res.data;
             let ratings = res.data.Ratings;
+            let siteLink = `<a href="https://www.imdb.com/title/${movieID}">Clique aqui para ir ao site</a>`;
 
             setImage('poster', `${data.Poster}`);
             setvalue('titulo', `${data.Title}`);
@@ -83,6 +129,7 @@ let loadModal = (id) => {
             ratings.forEach(item => {
                 set('avaliacoes', `${item.Source} ${item.Value}`)
             });
+            setvalue('link', `${siteLink}`);
 
         })
         .catch((err) => {
@@ -114,130 +161,46 @@ let carousel = () => {
         noWrap: false,
         duration: 1000,
         padding: 300
-        // onCycleTo: function (incomingElement) {
-
-        //     if (lastPage == 1)
-        //         return;
-
-        //     if (incomingElement.id === lastItem) {
-        //         document.getElementById('cards').innerHTML = '';
-        //         pages++;
-        //         message(`Você está na pagina ${pages}`, 200, 'rounded');
-        //         Pagination.Start();
-        //         axios.get(`http://www.omdbapi.com/?s=${movieName.value}&apikey=27ec0df9&type=movie&page=${pages}`)
-        //             .then((res) => {
-
-        //                 let data = res.data.Search;
-        //                 let element = (id, name, poster) => {
-        //                     return `<a id="${id}" class="carousel-item" href="#${id}"><h6 class="center white-text">${name}</h6>
-        //                 <img class="modal-trigger" href="#infomodal imagem" onclick="loadModal(${id})" onerror="this.src='/assets/warning.svg'" style="width: 300px; height: 370px; border-radius: 10px;"
-        //                  src="${poster}"></a>`;
-        //                 }
-
-        //                 data.forEach(item => {
-
-        //                     item.Poster == 'N/A' ? set('cards', `${element(item.imdbID, item.Title, item.Poster)}`) :
-        //                         set('cards', `${element(item.imdbID, item.Title, item.Poster)}`);
-
-        //                 });
-        //                 carousel();
-        //                 lastItem = data[data.length - 1].imdbID;
-
-
-        //             })
-        //             .catch((err) => {
-        //                 console.log(`Erro: ${err}`);
-        //             });
-        //     }
-        // }
     });
 }
 
-let getSearch = () => {
+let getMovies = () => {
 
-    document.getElementById('searchInput').addEventListener('keydown', (e) => {
+    validaCampo(movieName.value)
+    index++;
 
-        document.getElementById('cards').innerHTML = '';
-        if (e.keyCode === 8) {
-            index = 1;
+    axios.get(`http://www.omdbapi.com/?s=${movieName.value}&apikey=27ec0df9&type=movie`)
+        .then((res) => {
+
+            let data = res.data.Search;
+            jsonContent = res.data.Search;
+            lastPage = Math.ceil(parseInt(res.data.totalResults) / 10);
+
+            let element = (id, name, poster) => {
+                return `<a id="${id}" class="carousel-item" href="#${id}"><h6 class="center white-text">${name}</h6>
+                <img class="modal-trigger responsive-img" href="#infomodal" onclick="loadModal(${id})" onerror="this.src='/assets/warning.svg'" style="width: 300px; height: 370px; border-radius: 10px;"
+                 src="${poster}"></a>`;
+            }
+
+            data.forEach(item => {
+
+                item.Poster == 'N/A' ? set('cards', `${element(item.imdbID, item.Title, item.Poster)}`) :
+                    set('cards', `${element(item.imdbID, item.Title, item.Poster)}`);
+
+            });
+
+            lastItem = data[data.length - 1].imdbID;
+
+            carousel();
+            incre = 1;
             pages = 1;
-            lastItem = '';
-            document.getElementById('pagination').innerHTML = '';
-        }
+            init();
 
-        if (e.keyCode === 13) {
-
-            validaCampo(movieName.value)
-            message(`${lastPage} resultados para esta pesquisa`, 200, 'rounded');
-            index++;
-
-            axios.get(`http://www.omdbapi.com/?s=${movieName.value}&apikey=27ec0df9&type=movie`)
-                .then((res) => {
-
-                    let data = res.data.Search;
-                    jsonContent = res.data.Search;
-                    lastPage = Math.ceil(parseInt(res.data.totalResults) / 10);
-
-                    let element = (id, name, poster) => {
-                        return `<a id="${id}" class="carousel-item" href="#${id}"><h6 class="center white-text">${name}</h6>
-                        <img class="modal-trigger responsive-img" href="#infomodal" onclick="loadModal(${id})" onerror="this.src='/assets/warning.svg'" style="width: 300px; height: 370px; border-radius: 10px;"
-                         src="${poster}"></a>`;
-                    }
-
-                    data.forEach(item => {
-
-                        item.Poster == 'N/A' ? set('cards', `${element(item.imdbID, item.Title, item.Poster)}`) :
-                            set('cards', `${element(item.imdbID, item.Title, item.Poster)}`);
-
-                    });
-
-                    lastItem = data[data.length - 1].imdbID;
-
-                    carousel();
-                    incre = 1;
-                    pages = 1;
-
-                    init();
-
-                })
-                .catch((err) => {
-                    console.log(`Erro ${err}`);
-                });
-        }
-    });
-
+        })
+        .catch((err) => {
+            console.log(`Erro ${err}`);
+        });
 }
-
-function ConvertToCSV(objArray) {
-    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    let str = '';
-
-    for (let i = 0; i < array.length; i++) {
-        let line = '';
-        for (let index in array[i]) {
-            if (line != '') line += ','
-
-            line += array[i][index];
-        }
-
-        str += line + '\r\n';
-    }
-    return str;
-}
-
-function download(filename, text) {
-    let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
-
 
 let Pagination = {
 
@@ -249,30 +212,25 @@ let Pagination = {
         Pagination.step = 3;
     },
 
-    // add pages by number (from [s] to [f])
     Add: function (s, f) {
         for (let i = s; i < f; i++) {
             Pagination.code += `<a onclick="loadPage('${i}')">${i}</a>`;
         }
     },
 
-    // add last page with separator
     Last: function () {
         Pagination.code += `<i>. . . .</i><a>${Pagination.size}</a>`;
     },
 
-    // add first page with separator
     First: function () {
         Pagination.code += '<a>1</a><i>. . . .</i>';
     },
 
-    // change page
     Click: function () {
         pages = +this.innerHTML;
         Pagination.Start();
     },
 
-    // previous page
     Prev: function () {
         Pagination.page--;
         if (Pagination.page < 1) {
@@ -281,7 +239,6 @@ let Pagination = {
         Pagination.Start();
     },
 
-    // next page
     Next: function () {
         Pagination.page++;
         if (Pagination.page > Pagination.size) {
@@ -290,8 +247,6 @@ let Pagination = {
         Pagination.Start();
     },
 
-
-    // binding pages
     Bind: function () {
         let a = Pagination.e.getElementsByTagName('a');
         for (let i = 0; i < a.length; i++) {
@@ -300,14 +255,12 @@ let Pagination = {
         }
     },
 
-    // write pagination
     Finish: function () {
         Pagination.e.innerHTML = Pagination.code;
         Pagination.code = '';
         Pagination.Bind();
     },
 
-    // find pagination type
     Start: function () {
         if (Pagination.size < Pagination.step * 2 + 6) {
             Pagination.Add(1, Pagination.size + 1);
@@ -328,7 +281,6 @@ let Pagination = {
         Pagination.Finish();
     },
 
-    // create skeleton
     Create: function (e) {
 
         let html = ['<span></span>'];
@@ -337,15 +289,12 @@ let Pagination = {
         Pagination.e = e.getElementsByTagName('span')[0];
     },
 
-    // init
     Init: function (e, data) {
         Pagination.Extend(data);
         Pagination.Create(e);
         Pagination.Start();
     }
 };
-
-
 
 let init = function () {
     Pagination.Init(document.getElementById('pagination'), {
